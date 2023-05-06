c7292c1966dcdea0653b500e7edeabd54aa5f2dd
fredeugine <fredadomako2003@gmail.com>
2023-05-06 17:32:19 +0200

    Finsihing up

--- starter-code/apk.js
+++ starter-code/apk.js
@@ -18,19 +18,137 @@ function moveMarker(mouse) {
 container.addEventListener('mousemove', moveMarker);
 const clickCounts = [0, 0, 0, 0, 0, 0, 0];
 const div1CounterPositions = [455, 455, 455, 455, 455, 455, 455];
+const board = [
+	              [null,null,null,null,null,null,null], 
+               [null,null,null,null,null,null,null], 
+               [null,null,null,null,null,null,null], 
+               [null,null,null,null,null,null,null], 
+               [null,null,null,null,null,null,null], 
+               [null,null,null,null,null,null,null]
+               ];
+ 
+ var tile = [5,5,5,5,5,5,5]
+
+
+
+function checkHorizontal(board) {
+	var win = 0
+	var win2 = 0
+	for (var arr = 0; arr < board.length; arr++) {
+		for (var i = 0; i < board[arr].length; i++) {
+			if(board[arr][i] === 'x'){
+				win++
+			} else {
+				win = 0
+			}
+			if (board[arr][i] === 'y') {
+				win2++
+			} else {
+				win2 = 0
+			}
+			if (win === 4 || win2 === 4) {
+				return console.log("Horizontal win!")
+				
+			}
+		}
+	}
+
+	for (var i = 0; i <= 6; i++) {
+	for (var arr = 0; arr < board.length; arr++) {
+			if (board[arr][i] === 'x') {
+				win++
+			} else {
+				win = 0
+			}
+			if (board[arr][i] === 'y') {
+				win2++
+			} else {
+				win2 = 0
+			}
+			if (win === 4 || win2 === 4) {
+				return console.log("Horizontal win!")
+	
+			}
+		}
+	}
+	
+// Check for diagonal wins (left to right)
+for (var row = 0; row <= 2; row++) {
+	for (var col = 0; col <= 3; col++) {
+		swin = 0
+		swin2 = 0
+		for (var i = 0; i < 4; i++) {
+			if (board[row + i][col + i] === 'x') {
+				swin++
+			} else {
+				swin = 0
+			}
+			if (board[row + i][col + i] === 'y') {
+				swin2++
+			} else {
+				swin2 = 0
+			}
+			if (swin === 4 || swin2 === 4) {
+				return console.log("Diagonal win!")
+			}
+		}
+	}
+}
+
+// Check for diagonal wins (right to left)
+for (var row = 0; row <= 2; row++) {
+	for (var col = 3; col <= 6; col++) {
+		dwin = 0
+		dwin2 = 0
+		for (var i = 0; i < 4; i++) {
+			if (board[row + i][col - i] === 'x') {
+				dwin++
+			} else {
+				dwin = 0
+			}
+			if (board[row + i][col - i] === 'y') {
+				win2++
+			} else {
+				dwin2 = 0
+			}
+			if (dwin === 4 || dwin2 === 4) {
+				return console.log("Diagonal win!")
+			}
+		}
+	}
+}
+	
+	
+}
+
+
+	
 
 function invisible() {
   invisibleDivs.forEach(function(div, index) {
     div.addEventListener('click', function() {
+    	
       if (clickCounts[index] < 6) {
         clickCounts[index]++;
+       if(whoseTurn === 0){
+       board[tile[index]][index] = 'y';
+       tile[index]--;
+       }
+       else{
+       	board[tile[index]][index] = 'x';
+       	tile[index]--;
+       }
+       
+       
 
+
+      
         const newCounter = counter.cloneNode();
         newCounter.style.setProperty('transition', 'transform 700ms ease-in-out');
         newCounter.style.visibility = 'visible';
         container.appendChild(newCounter);
         newCounter.style.transform = `translatex(${(index * 87.9)}px)`;
-
+        checkHorizontal(board)
         setTimeout(function() {
           newCounter.style.transform = `translate(${(index * 87.9)}px, ${div1CounterPositions[index]}px)`;
           div1CounterPositions[index] -= 88;
@@ -79,6 +197,7 @@ function countDown() {
   
 }
 countDown()
+
 
 
 
